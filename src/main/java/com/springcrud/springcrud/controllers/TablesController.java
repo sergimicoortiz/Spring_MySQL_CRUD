598779b95590a0b0ae8a26af2c60983d6b5932aa
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2022-11-08 16:34:51 +0100

    detele and update

--- src/main/java/com/springcrud/springcrud/controllers/TablesController.java
+++ src/main/java/com/springcrud/springcrud/controllers/TablesController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.hibernate.annotations.Any;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +15,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springcrud.springcrud.model.Tables;
 import com.springcrud.springcrud.repository.TablesRepository;
-
-@SuppressWarnings("unused")
 
 @CrossOrigin(origins = "http://localhost:5001")
 @RestController
@@ -44,9 +40,24 @@ public class TablesController {
 
             return new ResponseEntity<>(tables, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }// get all
+
+    @GetMapping("tables/{id}")
+    public ResponseEntity<Tables> getOneTables(@PathVariable(required = true) Long id) {
+        try {
+            Optional<Tables> findTable = tablesRepository.findById(id);
+
+            if (findTable.isPresent()) {
+                return new ResponseEntity<>(findTable.get(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }// get one
 
     @PostMapping("tables")
     public ResponseEntity<Tables> createTable(@RequestBody Tables tables) {
@@ -59,9 +70,35 @@ public class TablesController {
             return new ResponseEntity<>(newTable, HttpStatus.OK);
 
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 
         }
-    }
+    }// create
 
+    @DeleteMapping("tables/{id}")
+    public ResponseEntity<HttpStatus> deleteTable(@PathVariable(required = true) Long id) {
+        try {
+            tablesRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }// delete
+
+    @PutMapping("tables/{id}")
+    public ResponseEntity<Tables> updateTable(@PathVariable(required = true) Long id,
+            @RequestBody Tables newTables) {
+        try {
+            Optional<Tables> findTable = tablesRepository.findById(id);
+            if (findTable.isPresent()) {
+                Tables updateTable = findTable.get();
+                updateTable.setName(newTables.getName());
+                return new ResponseEntity<>(tablesRepository.save(updateTable), HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }// update
 }
